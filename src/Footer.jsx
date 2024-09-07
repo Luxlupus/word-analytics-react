2bead0ff0e5ef16f4df6021ae2bb51eaeaae8ec6
Ivan Spiljak <ispiljak@gmail.com>
2024-09-07 12:20:53 +0200

    Created the main Components and Style

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyrights by LuxLupus. All rights reserved.</small>
+      <small>Last checked: 1 day ago</small>
+    </footer>
+  );
+}
