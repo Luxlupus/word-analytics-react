ef53687c92751b2ca9b9c3d3e434609630b4ae66
Ivan Spiljak <ispiljak@gmail.com>
2024-09-08 19:45:47 +0200

    Handling TextArea input and some basic validation

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function TextArea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  //deriving from state
+  const [warningText, setWarningText] = useState("");
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes("<script>")) {
+      setWarningText("No script tag allowed");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("No @ symbol allowed");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        placeholder="Enter your text"
+        spellCheck="false"
+        onChange={handleChange}
+      />
+      <Warning warningText={warningText} />
+    </div>
+  );
 }
